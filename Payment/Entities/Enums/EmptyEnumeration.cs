970a656af03d886e2ccae24409a19f52ed2c4bd7
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2019-02-04 15:40:58 -0200

    Adicionando Repositorio

---
+++ Payment/Entities/Enums/EmptyEnumeration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Payment.Entities.Enums
+{
+    public enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Payment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Payment.Entities;
+using Payment.Entities.Enums;
+namespace Payment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+
+
+
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.Processing.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Shipped");
+            Console.WriteLine(os);
+
+
+
+        }
+    }
+}
